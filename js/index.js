e4f29c992e219b3c69ffb3811d1a23e825bd6935
ashabahmed <ashabahmed7@gmail.com>
2020-10-09 19:11:34 -0400

    done

--- js/index.js
+++ js/index.js
@@ -7,50 +7,109 @@ document.addEventListener("DOMContentLoaded", () => {
 	const mainUrl = "http://localhost:3000/monsters/?_limit=50&_page="
 
   let pageNumber = 1
-
-  let url = mainUrl + pageNumber
-
-		const getMonsters = () => {
+  
+  
+  const getMonsters = (pageNumber) => {
+    let url = mainUrl + pageNumber
 			fetch(url)
 			.then(response => response.json())
 			.then(monsters => {
-        renderMonst(monsters)
+        renderMonsters(monsters)
 			})
 		}
 
+
+  const forward = document.querySelector('#forward')
+  const back = document.querySelector('#back')
+  const monstListDiv = document.getElementById('monster-container')
+
+  forward.addEventListener('click', function(e){
+    pageNumber++
+    monstListDiv.innerHTML = ''
+    getMonsters(pageNumber)
+  
+  })
+  back.addEventListener('click', function(e){
+    pageNumber--
+    monstListDiv.innerHTML = ''
+    getMonsters(pageNumber)
+  })
+
+		clickHandler()
     getMonsters()
     createForm()
   })
   
-  
-const renderMonst = (monsters) => {
-	const monstListDiv = document.getElementById('monster-container')
-		monsters.forEach(monster => {
-			let name = monster.name
-			let age = monster.age
-			let description = monster.description	
+  const renderMonsters = (monsters) => {
+  monsters.forEach(monster => {
+    renderMonst(monster)
+  })
 
-			let monstDiv = document.createElement('div')
-			monstDiv.innerHTML = `
-				<h2> Name: ${name} </h2>
-				<h3> Age: ${age} </h3>
-				<p> Description: ${description} </p>
-			`
-			monstListDiv.append(monstDiv)
-		});
+}
+  
+
+const renderMonst = (monster) => {
+	const monstListDiv = document.getElementById('monster-container')
+    let name = monster.name
+    let age = monster.age
+    let description = monster.description	
+
+    let monstDiv = document.createElement('div')
+    monstDiv.innerHTML = `
+      <h2> Name: ${name} </h2>
+      <h3> Age: ${age} </h3>
+      <p> Description: ${description} </p>
+    `
+    monstListDiv.prepend(monstDiv)
 }
 
 const createForm = () => {
+	const grabCreateMonstDiv = document.getElementById('create-monster')
   let form = document.createElement('form')
   form.innerHTML = `
     <label type='text' for='name'>Name</label>
-    <input for=name placeholde="name"></input>
-    <label  for=age></label>
-    <input type='text' for></input>
-    <label></label>
-    <input></input>
-    <input type="submit">Submit</input>
+    <input id='name' placeholder="name"></input>
+    <br>
+    <label for='age'>Age</label>
+    <input id='age' type='text' for='age'></input>
+    <br>
+    <label for='description'>Description</label>
+    <input id='description' ></input>
+    <br>
+    <input id='submit' type="submit"></input>
   `
+  grabCreateMonstDiv.append(form)
   
 }  
+
   
+const clickHandler = () => {
+	document.addEventListener('submit', e => {
+		e.preventDefault()
+      const form = e.target
+
+			const name = form.name.value
+			const age = form.age.value
+      const description = form.description.value
+			
+		const newMonstObj = {name: name, age: age, description: description}
+		
+		form.reset()
+
+		const options = {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				"accept": "application/json"
+			},
+			body: JSON.stringify(newMonstObj)
+		}
+
+		fetch("http://localhost:3000/monsters", options)
+		.then(response => response.json())
+		.then(monster => {
+			renderMonst(monster)
+    })
+  })
+
+}
